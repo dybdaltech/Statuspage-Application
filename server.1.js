3171af4743143b7ac50105fb9f378be3f3d48da6
dybdaltech <github234@dybdaltech.net>
2018-01-30 14:49:41 +0100

    rewrw

--- server.1.js
+++ server.1.js
@@ -2,136 +2,130 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const colors = require('colors');
 const path = require('path');
+const pug = require('pug');
+const cors = require('cors');
+const fs = require('fs');
+const jsonfile = require("jsonfile");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.set('view-engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(cors());
 
+/*
 status = {
     epost:{
+        name:'epost',
         state:"Unknown",
         color:""
     },
     intern:{
+        name:'intern',
         state:"Unknown",
         color:""
     },
     esa:{
+        name:'esa',
         state:"Unknown",
         color:""
     },
     helse:{
+        name:'helse',
         state:"Unknown",
         color:""
     }
 };
-
+*/
 info = {
     info:false,
-    info_0:{
-        message:"",
-        date:"",
-        color:""
-    },
-    info_1:{
-        message:"",
-        date:"",
-        color:""
-    },
-    info_2:{
-        message:"",
-        date:"",
-        color:""
-    }
+    infoTabs:[
+    ]
 };
-app.post('/state', (req, res ) => {
-    console.log(colors.green(req.ip + " Changed status too: "));
-    //console.log(req.body);
-    if(req.body.serviceChange === "epost") {
-        console.log(req.body.serviceChange + " -> " + req.body.epost.state);
-        status.epost.state = req.body.epost.state;
-        status.epost.color = req.body.epost.color;
-        res.send('Success');
+//JSON THINGS:
+let statusFile = './status.json';
+let epostFile = './status/epost.json';
+//statusFile = JSON.parse(statusFile);
+
+
+//ROUTES:
+app.post('/state/:srv', (req, res) => {
+    var stateToChange = req.params.srv;
+    if(stateToChange === "epost"){
+        console.log(colors.green(req.ip + " Set "+stateToChange + " To: "))
+        console.log(req.body.state + "\n" + req.body.color)
+        let objectOne = req.body.state;
+        jsonfile.writeFileSync(statusFile, objectOne, (err) => {
+            console.error(err);
+        });
+        /*
+        status.epost.state = req.body.state;
+        status.epost.color = req.body.color;
+        */
     }
-    if(req.body.serviceChange === "esa"){
-        console.log(req.body.serviceChange + " -> " + req.body.esa.state);
-        status.esa.state  = req.body.esa.state;
-        status.esa.color = req.body.esa.color;
-        res.send('Success');
+    if(stateToChange === "intern"){
+        console.log(colors.green(req.ip + " Set "+stateToChange + " To: "))
+        console.log(req.body.state + "\n" + req.body.color)
+        status.intern.state = req.body.state;
+        status.intern.color = req.body.color;
     }
-    if(req.body.serviceChange === "intern"){
-        console.log(req.body.serviceChange + " -> " + req.body.intern.state);
-        status.intern.state = req.body.intern.state;
-        status.intern.color = req.body.intern.color;
-        res.send('Success');
+    if(stateToChange === "esa"){
+        console.log(colors.green(req.ip + " Set "+stateToChange + " To: "))
+        console.log(req.body.state + "\n" + req.body.color)
+        status.esa.state = req.body.state;
+        status.esa.color = req.body.color;
     }
-    if(req.body.serviceChange === "helse"){
-        console.log(req.body.serviceChange + " -> " + req.body.helse.state);
-        status.helse.state = req.body.helse.state;
-        status.helse.color = req.body.helse.color;
-        res.send('Success');
-    } 
-    if(req.body.serviceChange === "all") {
-        console.log(req.body.serviceChange + " -> " + "All good?");
-        status.epost.state = req.body.epost.state;
-        status.epost.color = req.body.epost.color;
-        status.intern.state = req.body.intern.state;
-        status.intern.color = req.body.intern.color;
-        status.esa.state  = req.body.esa.state;
-        status.esa.color = req.body.esa.color;
-        status.helse.state = req.body.helse.state;
-        status.helse.color = req.body.helse.color;
-        res.send('Success');
+    if(stateToChange === "helse"){
+        console.log(colors.green(req.ip + " Set "+stateToChange + " To: "))
+        console.log(req.body.state + "\n" + req.body.color)
+        status.helse.state = req.body.state;
+        status.helse.color = req.body.color;
     }
-    else {
-        res.send("Failed to set state for: " + req.body.serviceChange);
-        console.log("Failed to set state for: " + req.body.serviceChange);
-    }
-    
 });
 
-app.post('/info', (req, res ) => {
-    console.log(info);
-    console.log(colors.green(req.ip + " Changed information too: "));
+
+app.post('/info/create', (req, res) => {
+    console.log(req.body);
     info.info = true;
-    info.message = req.body.message.message;
-    info.date = req.body.message.date;
-    info.color = req.body.message.color;
-    console.log(JSON.stringify(info))
-    res.send('Success');
-});
+    info.infoTabs.push({title:req.body.infoTitle, text:req.body.infoText, color:req.body.infoColor})
+    console.log(info.infoTabs);
+})
 
 app.get('/', (req, res) => {
     console.log("Get from: "+ req.ip);
-    res.render('index.ejs', {
-        state: status,
-        info: info
-    });
+    res.redirect('http://localhost:8080');
+    res.send('Redirected too correct site!');
 });
+
+app.delete('/api/info/:del', (req, res) => {
+    objectToDelete = req.params.del;
+    arr = info.infoTabs;
+    console.log(objectToDelete);
+    console.log('                 ');
+    for(a = 0; a < arr.length; a++){
+        console.log(arr[a].title);
+        if(arr[a].title === objectToDelete){
+            console.log('DELETING')
+            arr.splice(a, 1);
+            res.send('Success');
+            return;
+        } else {
+            res.send('Failed to delete object');
+        }
+    }
+});
+
+app.get('/api', (req, res) => {
+    status = jsonfile.readFileSync(statusFile);
+    console.log('API Fetch from: ' + req.ip);
+    res.json(status);
+});
+
+app.get('/api/info', (req, res) => {
+    console.log('API/INFO Fetch from: ' + req.ip);
+    res.json(info.infoTabs);
+})
 
 
 app.listen(PORT, () => console.log('Statuspage running on port: ' + colors.red(PORT)));
-
-
-
-<h4 >
-<% if (info.info) {%>
-    <p class="<%= info.info_0.color %>" >
-        <%= info.info_0.message %>
-        <%= info.info_0.date %>
-    </p>
-    <p class="<%= info.info_1.color%>" >
-        <%= info.info_1.message %>
-        <%= info.info_1.date %>
-    </p>
-    <p class="<%= info.info_2.color%>" >
-        <%= info.info_2.message %>
-        <%= info.info_2.date %>
-    </p>
-
-    <% } %>
-    
-</h4>
